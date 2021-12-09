a7a671a9dbe38179095ff4d7673c6441574bc2e1
dstephens179 <david.stephens365@gmail.com>
2021-12-08 19:52:12 -0500

    Dec 8 push.

---
+++ models/staging/All_Data.sql
@@ -0,0 +1,11 @@
+-- redirects to Store_Data dataset/schema, and overwrites All_Data master table
+{{ config(materialized='table', schema='Store_Data') }}
+
+SELECT *
+FROM {{ ref('current_output') }}
+WHERE Date is not Null
+
+UNION ALL
+SELECT *
+FROM {{ ref('historical_data') }}
+WHERE Date is not Null
