57d3c9e20c416e9b4b19fe5c5f0864b670c4582b
dstephens179 <david.stephens365@gmail.com>
2021-12-10 13:00:28 -0500

    commented on current as a GitLens test to GitHub.

--- models/staging/current_output.sql
+++ models/staging/current_output.sql
@@ -1,30 +1,30 @@
-{{ config(materialized='table') }}
+-- {{ config(materialized='table') }}
 
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_centro`
-WHERE Date is not Null
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_centro`
+-- WHERE Date is not Null
 
-UNION ALL
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_segovia`
-WHERE Date is not Null
+-- UNION ALL
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_segovia`
+-- WHERE Date is not Null
 
-UNION ALL
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_patria`
-WHERE Date is not Null
+-- UNION ALL
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_patria`
+-- WHERE Date is not Null
 
-UNION ALL
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_pasaje`
-WHERE Date is not Null
+-- UNION ALL
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_pasaje`
+-- WHERE Date is not Null
 
-UNION ALL
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_vallardo`
-WHERE Date is not Null
+-- UNION ALL
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_vallardo`
+-- WHERE Date is not Null
 
-UNION ALL
-SELECT *
-FROM `source-data-314320.joyeria_dataset.gs_va_en_linea`
-WHERE Date is not Null
+-- UNION ALL
+-- SELECT *
+-- FROM `source-data-314320.joyeria_dataset.gs_va_en_linea`
+-- WHERE Date is not Null
