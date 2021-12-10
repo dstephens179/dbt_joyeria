d51df0de4a7ec080508eda9ba2312b17c6b58b21
dstephens179 <david.stephens365@gmail.com>
2021-12-10 12:52:39 -0500

    uncommented as the GitHub passed the test.

--- models/staging/historical_data.sql
+++ models/staging/historical_data.sql
@@ -1,30 +1,30 @@
--- {{ config(materialized='table') }}
+{{ config(materialized='table') }}
 
--- SELECT *
--- FROM `source-data-314320.historical_data.Centro_2015_2021-10-31`
--- WHERE Date is not Null
+SELECT *
+FROM `source-data-314320.historical_data.Centro_2015_2021-10-31`
+WHERE Date is not Null
 
--- UNION ALL
--- SELECT *
--- FROM `source-data-314320.historical_data.Segovia_2017_2021-10-31`
--- WHERE Date is not Null
+UNION ALL
+SELECT *
+FROM `source-data-314320.historical_data.Segovia_2017_2021-10-31`
+WHERE Date is not Null
 
--- UNION ALL
--- SELECT *
--- FROM `source-data-314320.historical_data.Patria_2015_2021-10-31`
--- WHERE Date is not Null
+UNION ALL
+SELECT *
+FROM `source-data-314320.historical_data.Patria_2015_2021-10-31`
+WHERE Date is not Null
 
--- UNION ALL
--- SELECT *
--- FROM `source-data-314320.historical_data.Pasaje_2017_2021-10-31`
--- WHERE Date is not Null
+UNION ALL
+SELECT *
+FROM `source-data-314320.historical_data.Pasaje_2017_2021-10-31`
+WHERE Date is not Null
 
--- UNION ALL
--- SELECT *
--- FROM `source-data-314320.historical_data.Vallardo_2018_2021-10-31`
--- WHERE Date is not Null
+UNION ALL
+SELECT *
+FROM `source-data-314320.historical_data.Vallardo_2018_2021-10-31`
+WHERE Date is not Null
 
--- UNION ALL
--- SELECT *
--- FROM `source-data-314320.historical_data.VA_en_linea_2018_2021-10-31`
--- WHERE Date is not Null
+UNION ALL
+SELECT *
+FROM `source-data-314320.historical_data.VA_en_linea_2018_2021-10-31`
+WHERE Date is not Null
