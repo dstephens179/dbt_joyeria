09b129d7a1361c5709336359af954fd4ebf9ed37
dstephens179 <david.stephens365@gmail.com>
2022-01-08 16:06:02 -0500

    Updated to joyeria_dataset and away from agency_data_pipeline

--- models/staging/forecast.sql
+++ models/staging/forecast.sql
@@ -4,13 +4,13 @@ SELECT
     CAST(tienda AS STRING) AS tienda,
     CAST(NULL AS STRING) AS may_men,
     CAST(index AS DATE) AS date,
-    CAST("M&D" AS STRING) AS owner,
+    CAST(owner AS STRING) AS owner,
     CAST(NULL AS STRING) AS metal_type,
     CAST(NULL AS STRING) AS linea,
-    CAST(NULL AS STRING) AS product_type,
+    CAST(product_type AS STRING) AS product_type,
     CAST(NULL AS FLOAT64) AS sales,
     CAST(total_sales AS FLOAT64) AS forecast,
     CAST(NULL AS INT64) AS inventario,
     CAST(NULL AS STRING) AS nombre_cliente,
     CAST(NULL AS STRING) AS nombre_agente,
-FROM `source-data-314320.agency_data_pipeline.forecast`
+FROM `source-data-314320.joyeria_dataset.forecast`
