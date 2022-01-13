6db93507535ff06bc3b390637dc39f7ed63df2bf
dstephens179 <david.stephens365@gmail.com>
2022-01-13 12:11:26 -0500

    slight wording changes. Nothing major.

--- models/staging/All_Data.sql
+++ models/staging/All_Data.sql
@@ -1,16 +1,13 @@
--- redirects to Store_Data dataset/schema, and overwrites All_Data master table
+-- redirects to Store_Data dataset/schema, and overwrites All_Data
 {{ config(materialized='table', schema='Store_Data') }}
 
 SELECT *
 FROM {{ ref('current_output') }}
-WHERE Date is not Null
 
 UNION ALL
 SELECT *
 FROM {{ ref('historical_data') }}
-WHERE Date is not Null
 
 UNION ALL
 SELECT *
 FROM {{ ref('forecast') }}
-WHERE Date is not Null
