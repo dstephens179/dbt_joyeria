adfc7d3bd5a4d0786e5d9c4ea5ef7d66d87e6bfe
dstephens179 <david.stephens365@gmail.com>
2022-03-21 17:13:21 -0400

    updated with left_join on gold price.

--- models/staging/All_Data.sql
+++ models/staging/All_Data.sql
@@ -1,13 +1,22 @@
 -- redirects to Store_Data dataset/schema, and overwrites All_Data
 {{ config(materialized='table', schema='Store_Data') }}
 
-SELECT *
-FROM {{ ref('current_output') }}
+WITH All_Data as (
+    SELECT *
+    FROM {{ ref('current_output') }}
 
-UNION ALL
-SELECT *
-FROM {{ ref('historical_data') }}
+    UNION ALL
+    SELECT *
+    FROM {{ ref('historical_data') }}
 
-UNION ALL
-SELECT *
-FROM {{ ref('forecast') }}
+    UNION ALL
+    SELECT *
+    FROM {{ ref('forecast') }}
+)
+
+SELECT 
+    All_Data.*,
+    gold_price.* EXCEPT (date)
+FROM All_Data
+LEFT JOIN `source-data-314320.joyeria_dataset.gold_price` AS gold_price
+    ON All_Data.date = gold_price.date
